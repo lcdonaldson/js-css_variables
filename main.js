8a25a2d129e8d058fdb9c7fc3ac362f99eb7cad9
Levi Donaldson <lcdbacanas@gmail.com>
2018-01-16 00:42:38 -0600

    change color function

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const inputs = document.querySelectorAll('.controls input');
       const suffix = this.dataset.sizing || '';
       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     }
-
+    function colorChange(color){
+        document.body.style.background = color;
+    }
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+    
