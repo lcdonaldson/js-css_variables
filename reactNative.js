b7a7bf8d0896617761cf91a39be191d80e7f4474
Levi Donaldson <lcdbacanas@gmail.com>
2018-02-21 17:46:02 -0600

    initial start file (example)

---
+++ reactNative.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+const App = () => {
+  return <Text>Start</Text>;
+}
+
+AppRegistry.registerComponent('demo', () => App);
