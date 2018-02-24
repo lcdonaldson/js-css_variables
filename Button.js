71e9040d2abc43ccd08113d8e85f7cc7389326e8
Levi Donaldson <lcdbacanas@gmail.com>
2018-02-23 19:45:09 -0600

    Update Button.js

--- Button.js
+++ Button.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, TouchableOpacity,
+  StyleSheet } from 'react-native';
 
 const Button = () => {
-  <Text>click me</Text>
+  <View style={styles.container}>
+    <TouchableOpacity style={styles.button} onPress={this.onPress}>click me</TouchableOpacity>
+  </View>
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10
+  },
+
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
+  },
+});
 
 export default Button;
