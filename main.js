3b6b6aebfdc62520ad3995f8ee1c601d74a7192a
Levi Donaldson <lcdbacanas@gmail.com>
2018-03-30 20:56:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const inputs = document.querySelectorAll('.controls input');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
-    function colorChange(color){
-        document.body.style.background = color;
-    }
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+function colorChange(color){
+    document.body.style.background = color;
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
     
